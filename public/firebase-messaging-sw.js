32aebbcb3774f8bf7e927e8d55394d4ea88ec3e4
kumaranal <analbiswas180@gmail.com>
2024-07-15 23:49:44 +0530

    chage

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+// public/firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js');
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+// "use client";
+// import { useEffect } from "react";
+// import {
+//   requestNotificationPermission,
+//   onMessageListener,
+// } from "../components/firebaseMessaging";
+
 import { cookies } from "next/headers";
 import Image from "next/image";
 import Dashboard from "./(protected)/dashboard/page";
@@ -6,5 +13,29 @@ import SignIn from "./(public)/auth/signin/page";
 const isAuthenticated = cookies().get("authenticate")?.value || false;
 console.log("isAuthenticate", isAuthenticated);
 export default function Home() {
+  // useEffect(() => {
+  //   requestNotificationPermission();
+
+  //   onMessageListener()
+  //     .then((payload) => {
+  //       console.log("Message received. ", payload);
+  //       // Handle the message as per your needs
+  //     })
+  //     .catch((err) => console.log("Failed: ", err));
+
+  //   if ("serviceWorker" in navigator) {
+  //     navigator.serviceWorker
+  //       .register("/firebase-messaging-sw.js")
+  //       .then((registration) => {
+  //         console.log(
+  //           "Service Worker registered with scope:",
+  //           registration.scope
+  //         );
+  //       })
+  //       .catch((error) => {
+  //         console.error("Service Worker registration failed:", error);
+  //       });
+  //   }
+  // }, []);
   return <>{isAuthenticated ? <Dashboard /> : <SignIn />}</>;
 }
